e3166bd6163024f329ef36a4f95bf432b6b7143f
Luicil Fernandes <luicilfernandes@hotmail.com>
2015-07-23 08:04:35 -0300

    ProgressWindow
    ProgressWindow em andamento

---
+++ MacRAR/ProgressWindow/progressWindowController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Foundation;
+using AppKit;
+
+namespace MacRAR
+{
+	public class progressWindowController : NSObject 
+	{
+
+		[Export("window")]
+		public progressWindow  Window { get; set;}
+
+		[Outlet]
+		AppKit.NSProgressIndicator pb_Arquivos { get; set; }
+
+		[Outlet]
+		AppKit.NSTextField lbl_ProcArq { get; set; }
+
+		public string lblProcArq {
+			get { return lbl_ProcArq.StringValue ; }
+			set { lbl_ProcArq.StringValue = value; }
+		}
+
+		public void ShowSheet(NSWindow inWindow) {
+			NSApplication.SharedApplication.BeginSheet (Window, inWindow);
+		}
+
+		public void CloseSheet() {
+			NSApplication.SharedApplication.EndSheet (Window);
+			Window.Close();
+		}
+
+		[Export ("btn_actCancela:")]
+		void LoginCancel (NSObject sender) {
+			
+			CloseSheet();
+			//RaiseLoginCanceled ();
+		}
+
+		public progressWindowController ()
+		{
+			// Load the .xib file for the sheet
+			NSBundle.LoadNib ("progressWindow", this);
+		}
+	}
+}
+
